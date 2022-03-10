90a565172b33226b133d2a940661c70d00999b8c
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2022-03-10 08:53:55 +0700

    Adding models relations

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    public function admin(){
+        return $this->hasOne(User::class, 'id', 'admin_id');
+    }
+
+    public function transaction_item(){
+        return $this->hasMany(TransactionItem::class, 'transaction_id', 'id');
+    }
 }
--- app/Models/TransactionItem.php
+++ app/Models/TransactionItem.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class TransactionItem extends Model
 {
     use HasFactory;
+
+    public function item(){
+        return $this->hasOne(Item::class, 'id', 'item_id');
+    }
+
+    public function transaction(){
+        return $this->hasOne(Transaction::class, 'id', 'transaction_id');
+    }
 }
