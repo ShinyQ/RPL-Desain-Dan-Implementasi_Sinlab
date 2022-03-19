fa7d709802bef17ee8cc93a7d0989b5150d40c17
krobus00 <michaelglaxy@gmail.com>
2022-03-19 22:56:48 +0700

    [ADD] saran inventaris

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -6,49 +6,55 @@
 
     <div class="section-body">
         <div class="card">
-            @if(auth()->user()->role == 'super_user')
-            <div class="card-header">
+            @if (auth()->user()->role == 'super_user')
+                <div class="card-header d-flex justify-content-between">
                     <a href="{{ url('request/create') }}" class="btn btn-primary">+ Tambah Barang Inventaris</a>
-            </div>
+                    {{-- #TODO add export button here --}}
+                </div>
+            @endif
+            @if (auth()->user()->role == 'user')
+                <div class="card-header d-flex justify-content-between">
+                    <button class="btn btn-primary" id="btnRequest">+ Ajukan Inventaris</button>
+                </div>
             @endif
             <div class="card-body">
                 <div class="table-responsive">
-                    <table id="dataTable" class="table table-bordered table-md">
+                    <table id="dataTable" class="table-bordered table-md table">
                         <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Foto</th>
-                            <th>Nama Barang</th>
-                            <th>Deskripsi</th>
-                            <th>Jumlah</th>
-                            @if(auth()->user()->role == 'super_user')
-                                <th>Tanggal Dibuat</th>
-                            @endif
-                            <th>Action</th>
-                        </tr>
+                            <tr>
+                                <th>#</th>
+                                <th>Foto</th>
+                                <th>Nama Barang</th>
+                                <th>Deskripsi</th>
+                                <th>Jumlah</th>
+                                @if (auth()->user()->role == 'super_user')
+                                    <th>Tanggal Dibuat</th>
+                                @endif
+                                <th>Action</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        @forelse($items as $key => $item)
-                        <tr>
-                            <td>{{ $key+1 }}</td>
-                            <td><img width="100" src="{{ $item->photo }}" alt=""></td>
-                            <td>{{ $item->name }}</td>
-                            <td>{{ $item->description }}</td>
-                            <td>{{ $item->qty }}</td>
-                            @if(auth()->user()->role == 'super_user')
-                                <td>{{ $item->created_at }}</td>
-                            <td>
-                                <a href="#" class="btn btn-primary">Edit</a>
-                            </td>
-                            @else
-                                <td>
-                                    <a href="#" class="btn btn-primary">Pinjam</a>
-                                </td>
-                            @endif
-                        </tr>
-                        @empty
-                            Data Masih Kosong
-                        @endforelse
+                            @forelse($items as $key => $item)
+                                <tr>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td><img width="100" src="{{ $item->photo }}" alt=""></td>
+                                    <td>{{ $item->name }}</td>
+                                    <td>{{ $item->description }}</td>
+                                    <td>{{ $item->qty }}</td>
+                                    @if (auth()->user()->role == 'super_user')
+                                        <td>{{ $item->created_at }}</td>
+                                        <td>
+                                            <a href="#" class="btn btn-primary">Edit</a>
+                                        </td>
+                                    @else
+                                        <td>
+                                            <a href="#" class="btn btn-primary">Pinjam</a>
+                                        </td>
+                                    @endif
+                                </tr>
+                            @empty
+                                Data Masih Kosong
+                            @endforelse
                         </tbody>
                     </table>
                 </div>
@@ -56,3 +62,98 @@
         </div>
     </div>
 @endsection
+
+
+<div class="modal fade show" tabindex="-1" role="dialog" id="modalRequest">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalRequestTitle">Saran Inventaris</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="formReview" data-id="">
+                    <div class="form-group">
+                        <label>Nama Barang</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="namaBarang" placeholder="nama barang">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Deskripsi Barang</label>
+                        <div class="input-group">
+                            <textarea class="form-control h-25" id="deskripsiBarang" rows="3" placeholder="deskripsi barang"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Jumlah Barang</label>
+                        <div class="input-group w-50">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-primary" type="button" id="decQty" disabled><i class="fa fa-minus" aria-hidden="true"></i></button>
+                            </div>
+                            <input type="text" class="form-control" id="qtyBarang" placeholder="1" disabled>
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="button" id="incQty"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+            <div class="modal-footer bg-whitesmoke br">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary" id="btnAjukan">Ajukan</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $('#btnRequest').on('click', function(event) {
+                $('#modalRequest').modal('show');
+                $('#namaBarang').val('')
+                $('#deskripsiBarang').val('')
+                $('#qtyBarang').val('1')
+            });
+            $('#decQty').on("click", function(event) {
+                let current = parseInt($('#qtyBarang').val())
+                $('#qtyBarang').val(current - 1 > 1 ? --current : 1);
+                if (current - 1 <= 1) {
+                    $('#decQty').attr("disabled", true);
+                }
+            })
+            $('#incQty').on("click", function(event) {
+                let current = parseInt($('#qtyBarang').val())
+                $('#qtyBarang').val(++current);
+                $('#decQty').attr("disabled", false);
+            })
+            $('#btnAjukan').on("click", function(event) {
+                $.ajax({
+                    url: `request`,
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    method: 'POST',
+                    data: {
+                        name: $('#namaBarang').val(),
+                        description: $('#deskripsiBarang').val(),
+                        qty: $('#qtyBarang').val(),
+                    },
+                    success: function(data) {
+                        $('#modalRequest').toggleClass('modal-progress');
+                        location.reload();
+                    },
+                    error: function(data) {
+                        $('#modalRequest').toggleClass('modal-progress');
+                        alert('Internal Server Error');
+                    }
+
+                });
+            })
+        });
+    </script>
+@endpush
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,19 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/favicon.png') }}">
     <title>Dashboard | {{ $title }}</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://demo.getstisla.com/assets/modules/datatables/datatables.min.css">
-    <link rel="stylesheet" href="https://demo.getstisla.com/assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="https://demo.getstisla.com/assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css">
-
     <!-- Template CSS -->
+
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/datatables.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/select.bootstrap4.min.css') }}">
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 </head>
@@ -29,8 +30,7 @@
                 <form class="form-inline mr-auto">
                     <ul class="navbar-nav mr-3">
                         <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
-                        <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a>
-                        </li>
+                        <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
                     </ul>
                 </form>
                 <a style="margin-top: 30px" href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
@@ -64,14 +64,14 @@
                             <li class="menu-header">Menu Utama</li>
                             <li class="active">
                                 <a class="nav-link" href="{{ url('/') }}">
-                                    <i class="fa fa-chart-bar"></i>
+                                    <i class="fas fa-chart-bar"></i>
                                     <span>Halaman Dashboard</span>
                                 </a>
                             </li>
 
                             <li>
                                 <a class="nav-link" href="{{ url('admin/user') }}">
-                                    <i class="fa fa-users"></i>
+                                    <i class="fas fa-users"></i>
                                     <span>Daftar Pengguna</span>
                                 </a>
                             </li>
@@ -79,21 +79,21 @@
                             <li class="menu-header">Menu Inventaris</li>
                             <li>
                                 <a class="nav-link" href="{{ url('admin/item') }}">
-                                    <i class="fa fa-box"></i>
+                                    <i class="fas fa-box"></i>
                                     <span>Inventaris Barang</span>
                                 </a>
                             </li>
 
                             <li>
                                 <a class="nav-link" href="{{ url('admin/transaction') }}">
-                                    <i class="fa fa-book"></i>
+                                    <i class="fas fa-book"></i>
                                     <span>Peminjaman Barang</span>
                                 </a>
                             </li>
 
                             <li>
                                 <a class="nav-link" href="{{ url('admin/request') }}">
-                                    <i class="fa fa-comment"></i>
+                                    <i class="fas fa-comment"></i>
                                     <span>Permintaan Barang</span>
                                 </a>
                             </li>
@@ -101,21 +101,21 @@
                             <li class="menu-header">Menu Peminjaman</li>
                             <li class="active">
                                 <a class="nav-link" href="{{ url('item') }}">
-                                    <i class="fa fa-box"></i>
+                                    <i class="fas fa-box"></i>
                                     <span>Daftar Inventaris</span>
                                 </a>
                             </li>
 
                             <li>
                                 <a class="nav-link" href="{{ url('transaction') }}">
-                                    <i class="fa fa-book"></i>
+                                    <i class="fas fa-book"></i>
                                     <span>Halaman Peminjaman</span>
                                 </a>
                             </li>
 
                             <li>
                                 <a class="nav-link" href="{{ url('request') }}">
-                                    <i class="fa fa-comment"></i>
+                                    <i class="fas fa-comment"></i>
                                     <span>Permintaan Barang</span>
                                 </a>
                             </li>
@@ -123,13 +123,13 @@
                             <li class="menu-header">Menu Peminjaman</li>
                             <li class="active">
                                 <a class="nav-link" href="{{ url('/') }}">
-                                    <i class="fa fa-box"></i>
+                                    <i class="fas fa-box"></i>
                                     <span>Daftar Inventaris</span>
                                 </a>
                             </li>
                             <li>
                                 <a class="nav-link" href="{{ url('transaction') }}">
-                                    <i class="fa fa-book"></i>
+                                    <i class="fas fa-book"></i>
                                     <span>Halaman Peminjaman</span>
                                 </a>
                             </li>
