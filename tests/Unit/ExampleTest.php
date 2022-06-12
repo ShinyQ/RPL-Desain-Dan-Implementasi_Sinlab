9aac5605d37a9ac36ce4f64f9d349daef43efa6a
krobus00 <michaelglaxy@gmail.com>
2022-06-12 07:48:46 +0700

    [ADD] unit test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
-
+// use PHPUnit\Framework\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use App\Models\User;
+use Tests\TestCase;
+// use Illuminate\Http\UploadedFile;
 class ExampleTest extends TestCase
 {
     /**
@@ -11,8 +15,46 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_that_true_is_true()
+    public function test_success_update_user()
     {
-        $this->assertTrue(true);
+        Storage::fake('public');
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $file = UploadedFile::fake()->create('random.jpeg');
+        $file->mimeType = 'image/jpeg';
+        $res = $this->put('user/user/1', [
+            'phone' => '082234890710',
+            'ktm' => $file,
+        ]);
+
+        $res->assertStatus(200);
+    }
+    public function test_exception_failed_update_user()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $res = $this->put('user/user/1', [
+            'phone' => '082234890710',
+        ]);
+        $res->assertStatus(500);
+    }
+    public function test_invalid_ktm_update_user()
+    {
+        Storage::fake('public');
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $file = UploadedFile::fake()->create('random.jpeg');
+        $file->mimeType = 'application/json';
+        $res = $this->put('user/user/1', [
+            'phone' => '082234890710',
+            'ktm' => $file,
+        ]);
+
+        $res->assertStatus(200);
     }
 }
